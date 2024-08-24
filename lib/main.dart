f93c94652a70dbd5d69a36271f7a1b8132a5dfb8
NamalTharindu97 <namal@Namals-MacBook-Pro.local>
2024-08-25 00:29:11 +0530

    container logics

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(myApp());
 }
 
-class MyApp extends StatelessWidget {
+class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'flutter demo',
+      title: "flutter demo 2",
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.orange,
+      ),
       home: MyHomePage(),
-      theme: ThemeData(primarySwatch: Colors.blue),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return SafeArea(
-      child: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Demo App'),
-            backgroundColor: Colors.blue,
-            leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-            actions: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))
-            ],
-            flexibleSpace: Image.asset(
-              "lib/assets/back.jpg",
-              fit: BoxFit.cover,
-            ),
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.directions_car),
-                  text: "tab one",
-                ),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
-              ],
-            ),
-            elevation: 20.0,
-          ),
-          body: TabBarView(children: [
-            tab1(),
-            Icon(Icons.directions_train),
-            Icon(Icons.directions_bike),
-          ]),
+    return (Scaffold(
+      appBar: AppBar(
+        title: Text("demo 2"),
+      ),
+      body: Container(
+        height: 200.0,
+        width: double.infinity,
+        margin: EdgeInsets.all(20.0),
+        //  padding: EdgeInsets.symmetric(horizontal: 20.0),
+        padding: EdgeInsets.only(top: 20.0),
+        //this only effect to the childs inside the container
+        alignment: Alignment.bottomCenter,
+        color: Colors.pink,
+        child: Text(
+          "this is the buity",
+          style: TextStyle(fontSize: 30.0),
         ),
       ),
-    );
+    ));
   }
-}
-
-//or can create custom widget and call it in the TabBarView
-Widget tab1() {
-  return Container(
-    child: Center(
-      child: Text("I m the Tab One"),
-    ),
-  );
 }
