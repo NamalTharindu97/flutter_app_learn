ef26eca11cfb7ea8c6b4778e980e2f8214f04bf9
NamalTharindu97 <namal@Namals-MacBook-Pro.local>
2024-08-24 17:33:44 +0530

    bottom app bar logics

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'flutter demo',
       home: MyHomePage(),
       theme: ThemeData(primarySwatch: Colors.blue),
@@ -21,8 +22,10 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
+      child: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
             title: Text('Demo App'),
             backgroundColor: Colors.blue,
             leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
@@ -33,8 +36,35 @@ class MyHomePage extends StatelessWidget {
             flexibleSpace: Image.asset(
               "lib/assets/back.jpg",
               fit: BoxFit.cover,
-            )),
+            ),
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.directions_car),
+                  text: "tab one",
+                ),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+            elevation: 20.0,
+          ),
+          body: TabBarView(children: [
+            tab1(),
+            Icon(Icons.directions_train),
+            Icon(Icons.directions_bike),
+          ]),
+        ),
       ),
     );
   }
+}
+
+//or can create custom widget and call it in the TabBarView
+Widget tab1() {
+  return Container(
+    child: Center(
+      child: Text("I m the Tab One"),
+    ),
+  );
 }
